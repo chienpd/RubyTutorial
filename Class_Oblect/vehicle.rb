826142ae293a572eae49efa391eb2461d3c9e46c
chienphamk58 <phamdinhchienk58@gmail.com>
2017-06-27 11:07:06 +0700

    Classes and Object

---
+++ Class_Oblect/vehicle.rb
@@ -0,0 +1,17 @@
+class Vehicle
+  Number no_of_wheels
+  Number horsepower
+  Characters type_of_tank
+  Number Capacity
+  Function speeding{
+
+  }
+
+  Function driving{
+
+  }
+
+  Function haltin{
+    
+  }
+end
